30cd07f3998d16b12b30ee7af7b1a2ae627e77f3
Onur Koçak <98240992+onurkck34@users.noreply.github.com>
2022-03-21 20:58:26 +0300

    Add files via upload

---
+++ traficlight.py
@@ -0,0 +1,10 @@
+lights = ["red","yellow","pink"]
+currentLight = lights[2]
+print(currentLight)
+
+if currentLight == "red":
+    print("STOP!")
+elif currentLight == "yellow":
+    print("READY!")
+else: 
+    print("GO!")
